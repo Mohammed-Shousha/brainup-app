f1c7b7e9d9c55fd940b586948aa7bac8f9a76d08
Mohammed Shousha <mshousha29@gmail.com>
2023-08-22 23:22:39 +0300

    implementing usecases

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -1,4 +1,13 @@
 const API_URL = "https://brainup-api.mazenamir.com/api";
 
-export const LOGIN_URL = `${API_URL}/auth/login`;
-export const REGISTER_URL = `${API_URL}/auth/register`;
+const AUTH_URL = `${API_URL}/auth`;
+
+export const LOGIN_URL = `${AUTH_URL}/login`;
+export const REGISTER_URL = `${AUTH_URL}/register`;
+
+export const SEND_CONFIRM_EMAIL_URL = `${AUTH_URL}/confirm-email`;
+export const VERIFY_CONFIRM_EMAIL_URL = `${SEND_CONFIRM_EMAIL_URL}/verify-code`;
+
+export const SEND_RESET_PASSWORD_URL = `${AUTH_URL}/forgot-password`;
+export const RESET_PASSWORD_URL = `${AUTH_URL}/reset-password`;
+export const VERIFY_RESET_PASSWORD_URL = `${RESET_PASSWORD_URL}/verify-code`;
--- src/data/repositories/user.repository.impl.ts
+++ src/data/repositories/user.repository.impl.ts
@@ -1,29 +1,32 @@
 import User from "../../domain/entities/user.entity";
 import IUserRepository from "../../domain/repositories/user.repository";
 
-import { LOGIN_URL, REGISTER_URL } from "../../core/utils/constants";
+import { sendRequest } from "../../core/utils/helpers";
+
+import {
+  SEND_CONFIRM_EMAIL_URL,
+  SEND_RESET_PASSWORD_URL,
+  LOGIN_URL,
+  REGISTER_URL,
+  VERIFY_CONFIRM_EMAIL_URL,
+  VERIFY_RESET_PASSWORD_URL,
+  RESET_PASSWORD_URL,
+} from "../../core/utils/constants";
 
 export class UserRepository implements IUserRepository {
   constructor() {}
 
   async register(user: User): Promise<User> {
     const { username, name, email, password, phone } = user;
-    const result = await fetch(REGISTER_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        username,
-        name,
-        email,
-        password,
-        user_type: "student",
-        phone_number: phone,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
 
-    const data = await result.json();
+    const data = await sendRequest(REGISTER_URL, "POST", {
+      username,
+      name,
+      email,
+      password,
+      user_type: "student",
+      phone_number: phone,
+    });
 
     console.log(data);
 
@@ -31,18 +34,53 @@ export class UserRepository implements IUserRepository {
   }
 
   async login(username: string, password: string): Promise<void> {
-    const result = await fetch(LOGIN_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        user: username,
-        password,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
+    const data = await sendRequest(LOGIN_URL, "POST", {
+      user: username,
+      password,
     });
 
-    const data = await result.json();
+    console.log(data);
+  }
+
+  async sendConfirmEmail(email: string): Promise<void> {
+    const data = await sendRequest(SEND_CONFIRM_EMAIL_URL, "POST", {
+      email,
+    });
+
+    console.log(data);
+  }
+
+  async verifyConfirmCode(code: string): Promise<void> {
+    const data = await sendRequest(VERIFY_CONFIRM_EMAIL_URL, "POST", {
+      code,
+    });
+
+    console.log(data);
+  }
+
+  async sendResetPasswordEmail(email: string): Promise<void> {
+    const data = await sendRequest(SEND_RESET_PASSWORD_URL, "POST", {
+      email,
+    });
+
+    console.log(data);
+  }
+
+  async verifyResetPasswordCode(code: string): Promise<string> {
+    const data = await sendRequest(VERIFY_RESET_PASSWORD_URL, "POST", {
+      code,
+    });
+
+    console.log(data);
+
+    return data.token;
+  }
+
+  async resetPassword(token: string, password: string): Promise<void> {
+    const data = await sendRequest(RESET_PASSWORD_URL, "POST", {
+      token,
+      password,
+    });
 
     console.log(data);
   }
