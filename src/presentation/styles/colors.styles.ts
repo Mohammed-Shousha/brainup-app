12dc8f5c45bc991fa4c91a30ad60a2fb8c84d2d6
Mohammed Shousha <mshousha29@gmail.com>
2023-09-13 03:32:03 +0300

    updating colors

--- src/presentation/styles/colors.styles.ts
+++ src/presentation/styles/colors.styles.ts
@@ -1,12 +1,17 @@
 const colors = {
   primary: "#312880",
   lightPrimary: "#5457A6",
-  secondary: "#000",
+  extraLightPrimary: "#9193B5",
+  secondary: "#F8B93F",
+  lightSecondary: "#FFDC9B",
   white: "#fff",
   black: "#000",
+  darkGray: "#232323",
   gray: "#787878",
   lightGray: "#808080",
   extraLightGray: "#e8e8e8",
+  ultraLightGray: "#bebebe",
+  wheat: "#f3f3f5",
   error: "#ff0000",
   success: "#00ff00",
 } as const;
