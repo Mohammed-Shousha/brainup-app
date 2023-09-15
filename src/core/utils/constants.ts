7c46aa39ce22840e27c29223c09693dd098c7c66
Mohammed Shousha <mshousha29@gmail.com>
2023-09-15 23:26:23 +0300

    updating constants

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -20,11 +20,9 @@ export const CLASSROOMS_URL = `${API_URL}/classrooms`;
 export const TEACHER_CLASSROOMS_URL = `${CLASSROOMS_URL}/teacher`;
 
 export const TEACHER_CREATE_CLASSROOM_URL = `${TEACHER_CLASSROOMS_URL}/create`;
-
 export const TEACHER_GET_CLASSROOMS_URL = `${TEACHER_CLASSROOMS_URL}/get`;
 export const TEACHER_GET_CLASSROOM_URL = (id: string) =>
   `${TEACHER_GET_CLASSROOMS_URL}/${id}`;
-
 export const TEACHER_CLASSROOM_APPROVE_URL = `${TEACHER_CLASSROOMS_URL}/approve`;
 export const TEACHER_CLASSROOM_REJECT_URL = `${TEACHER_CLASSROOMS_URL}/reject`;
 
@@ -32,15 +30,27 @@ export const TEACHER_CLASSROOM_REJECT_URL = `${TEACHER_CLASSROOMS_URL}/reject`;
 export const STUDENT_CLASSROOMS_URL = `${CLASSROOMS_URL}/student`;
 
 export const STUDENT_GET_CLASSROOMS_URL = `${STUDENT_CLASSROOMS_URL}/get`;
-
+export const STUDENT_GET_CLASSROOM_URL = (id: string) =>
+  `${STUDENT_GET_CLASSROOMS_URL}/${id}`;
 export const STUDENT_JOIN_CLASSROOM_URL = `${STUDENT_CLASSROOMS_URL}/join`;
 
 // Quizzes
+export const QUIZZES_URL = `${API_URL}/quizes`;
 
 // teacher
-
-export const TEACHER_QUIZZES_URL = `${API_URL}/quizes/teacher`;
+export const TEACHER_QUIZZES_URL = `${QUIZZES_URL}/teacher`;
 
 export const TEACHER_CREATE_QUIZ_URL = `${TEACHER_QUIZZES_URL}/create`;
-
 export const TEACHER_GET_QUIZZES_URL = `${TEACHER_QUIZZES_URL}/get`;
+
+// student
+export const STUDENT_QUIZZES_URL = `${QUIZZES_URL}/student`;
+
+export const STUDENT_GET_QUIZZES_URL = `${STUDENT_QUIZZES_URL}/get`;
+export const STUDENT_GET_QUIZ_URL = (id: string) =>
+  `${STUDENT_GET_QUIZZES_URL}/${id}`;
+
+// AI
+export const AI_URL = `${API_URL}/ai`;
+
+export const AI_GET_RESPONSE_URL = `${AI_URL}/get-response`;
